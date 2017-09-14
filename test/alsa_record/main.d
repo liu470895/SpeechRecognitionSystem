eb41a15237b87c847e3da5997af28dc36110b56b
jtortoise <jtortoise_xl@126.com>
2017-09-14 04:40:19 -0700

    删除编译生成的文件

--- test/alsa_record/main.d
+++
@@ -1,6 +0,0 @@
-main.o main.d: main.c inc/alsa/asoundlib.h inc/alsa/asoundef.h \
- inc/alsa/version.h inc/alsa/global.h inc/alsa/input.h inc/alsa/output.h \
- inc/alsa/error.h inc/alsa/conf.h inc/alsa/pcm.h inc/alsa/rawmidi.h \
- inc/alsa/timer.h inc/alsa/hwdep.h inc/alsa/control.h inc/alsa/mixer.h \
- inc/alsa/seq_event.h inc/alsa/seq.h inc/alsa/seqmid.h \
- inc/alsa/seq_midi_event.h inc/alsa/pcm_ioplug.h
