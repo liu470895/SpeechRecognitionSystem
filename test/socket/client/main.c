2470416f27dce90a51769a04f5b6ce6281199170
jtortoise <jtortoise_xl@126.com>
2017-09-18 05:22:36 -0700

    简单的socket连接测试

--- test/socket/client/main.c
+++ test/socket/client/main.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <strings.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main()
+#define BUFF_SIZE 128
+
+void usage(int argc, char *argv[])
 {
-	printf("Client.\n");
+	if(argc != 3)
+	{
+		fprintf(stderr, "usage: %s <IP> <PORT>\n", argv[0]);
+		exit(1);
+	}
+}
+int main(int argc, char *argv[])
+{
+	usage(argc, argv);
+
+	int sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sfd == -1)
+	{
+		perror("socket() error");
+		exit(1);
+	}
+	
+	struct sockaddr_in srv;
+	int srv_len = sizeof srv;
+	bzero(&srv, srv_len);
+
+	/*转换服务器的ip地址为网络字节序*/
+	srv.sin_family = AF_INET;
+	srv.sin_port = htonl(atoi(argv[2])); 
+	srv.sin_addr.s_addr = inet_addr(argv[1]); //172.xx.xx.xx
+
+	int err = connect(sfd, (struct sockaddr*)&srv, srv_len);
+	if(err == -1)
+	{
+		fprintf(stderr, "connet() to %s:%s error", argv[1], argv[2]);
+		perror(NULL);
+		exit(1);
+	}
+
+	
+	int n;
+	char *buf = (char *)malloc(BUFF_SIZE);
+	while(1)
+	{
+		bzero(buf, BUFF_SIZE);
+		n = read(sfd, buf, BUFF_SIZE-1);
+		printf("read=%s\n", buf);
+		write(sfd, buf, n);
+	}
+	
+
+	close(sfd);
+
 	return 0;
 }
--- test/socket/server/main.c
+++ test/socket/server/main.c
@@ -1,8 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <strings.h>
+#include <errno.h>
+#include <string.h>
 
-int main()
+#define BUFF_SIZE 128
+
+void usage(int argc, char *argv[])
 {
-	printf("Server.\n");
+	if(argc != 2)
+	{
+		fprintf(stderr, "usage: %s <PORT>.\n", argv[0]);
+		exit(1);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	
+	usage(argc, argv);
+	int sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sfd == -1)
+	{
+		perror("socket() error");
+		exit(1);
+	}
+
+	struct sockaddr_in srv;
+	int srv_len = sizeof srv;
+	bzero(&srv, srv_len);
+
+	/*设置本地IP地址和端口参数*/
+	srv.sin_family = AF_INET;
+	srv.sin_port = htons(atoi(argv[1]));
+	srv.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int err = bind(sfd, (struct sockaddr*)&srv, srv_len);
+	if(err == -1)
+	{
+		fprintf(stderr, "bind() PORT:%s error.\n", argv[1]);
+		exit(1);
+	}
+
+	err = listen(sfd, 3);
+	if(err == -1)
+	{
+		perror("listen() error");
+		exit(1);
+	}
+
+	struct sockaddr_in cli;
+	int cli_len;
+	bzero(&cli, sizeof cli);
+
+	printf("watting someboby connect.\n");
+	int cfd = accept(sfd, (struct sockaddr*)&cli, &cli_len);
+	if(cfd == -1)
+	{
+		perror("accept() error");
+		exit(1);
+	}
+
+	printf("%s connect success\n", inet_ntoa(cli.sin_addr));
+
+	int n;
+	char *buf = (char *)malloc(BUFF_SIZE);
+	char *msg = "Hello";
+	while(1)
+	{
+		write(cfd, msg, strlen(msg));	
+		bzero(buf, BUFF_SIZE);
+		n = read(cfd, buf, BUFF_SIZE-1);
+
+		printf("read %dbytes:%s\n", n, buf);
+	}
 	return 0;
 }
